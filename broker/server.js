73f9815396d7f7b361536e6ec8e3cacb6702e2a9
José Ignacio Alamos <jialamos@uc.cl>
2017-05-29 14:47:01 -0400

    update to new broker

--- broker/server.js
+++ broker/server.js
@@ -1,21 +1,66 @@
-const coap    = require('coap') // or coap
+const coap    = require('../') // or coap
     , server  = coap.createServer({type:'udp6'})
+const os = require('os')
+
+
+const Influx = require('influx');
+
+const influx = new Influx.InfluxDB({
+  host: '192.168.2.50',
+  database: 'iop',
+  schema: [
+    {
+      measurement: 'radio_data',
+      fields: {
+        airhum: Influx.FieldType.INTEGER,
+        soilhum: Influx.FieldType.INTEGER,
+        temp: Influx.FieldType.INTEGER,
+        light: Influx.FieldType.INTEGER
+      },
+      tags: [
+        'id'
+      ]
+    }
+  ]
+})
+
+influx.getDatabaseNames()
+  .then(names => {
+      console.log(names);
+    if (!names.includes('iop')) {
+      return influx.createDatabase('iop')
+    }
+})
+
 
 function process_data(req, res)
 {
     var data = [0,0,0,0];
-    var input = req.payload;
+    var input = req.payload.slice(4,12);
+    var id = req.payload.slice(0,4).join("");
+    console.log(req.payload);
     var js = {};
     for(i=0;i<4;i++)
     {
         data[i] = input[2*i]*255 + input[2*i+1]; 
     }
-    js["temp"] = data[0];
-    js["air_humidity"] = data[1];
-    js["light"] = data[2];
-    js["soil_humidity"] = data[3];
-
-    console.log(JSON.stringify(js));
+    console.log(data);
+    influx.writePoints([
+      {
+        measurement: 'radio_data',
+        tags: { id: id },
+        fields: { temp: data[0], airhum: data[1], light: data[2], soilhum: data[3] },
+      }
+    ]).then(() => {
+      return influx.query(`
+        select * from radio_data
+        where id = ${Influx.escape.stringLit(os.hostname())}
+        order by time desc
+        limit 1
+      `)
+    }).then(rows => {
+      rows.forEach(row => console.log(`Temp: ${row.temp}.\n Air hum: ${row.airhum}. \n Light: ${row.light}.\n Soil humidity: ${row.soilhum}.\n\n`))
+    })
 }
 server.on('request', function(req, res) {
   process_data(req, res);
